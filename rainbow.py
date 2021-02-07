b430e301926ca3163afb5ce8a9a463f2451e58cd
_northernlights_ <clem@lorteau.fr>
2021-02-07 14:13:40 -0800

    Create rainbow.py

---
+++ rainbow.py
@@ -0,0 +1,24 @@
+__module_name__ = 'Rainbow'
+__module_version__ = '0.1'
+__module_description__ = 'Send a colorful message'
+
+import hexchat
+
+def r(word, word_eol, userdata):
+    text = word_eol[1]
+    newtext = ''
+
+    #cycle through mIRC colors (https://www.mirc.com/colors.html) 2 to 13 and apply to
+    #characters one by one, prepended with ascii character 3, skipping space
+    i = 2
+    for c in text:
+        if (c != ' '):
+            c = chr(3) + str(i) + c
+            i+=1
+            if (i == 14): i = 2
+        newtext = newtext + c
+
+    hexchat.command('say ' + newtext)
+
+print('Rainbox text script loaded')
+hexchat.hook_command('r', r, help='usage: /r text - Sends [text] in multiple colors')
